70b67977dc2223e3eb995a087baf2782e468b589
wendamys <wendamys@yandex.ru>
2026-05-06 15:48:54 +0700

    complete, modified Exercise3.py

--- Exercise3.py
+++ Exercise3.py
@@ -1,6 +1,21 @@
-from pyexpat.errors import messages
-from typing import Protocol
+from typing import Protocol, runtime_checkable
 from abc import ABC, abstractmethod
+
+@runtime_checkable
+class Harbinger(Protocol):
+    @abstractmethod
+    def chill(self) -> str: ...
+
+@runtime_checkable
+class Omnivore(Protocol):
+    @abstractmethod
+    def hunt(self) -> str: ...
+
+
+def print_pets(args):
+    sorted_pets = sorted(args, key=lambda x: isinstance(x, Omnivore))
+    for animal in sorted_pets:
+        print(animal)
 
 class Exercise3:
 
@@ -15,7 +30,8 @@ class Exercise3:
         items_animals = []
         while len(items_animals) < user_num:
             user_input_animal = input()
-            if user_input_animal == "dog" or user_input_animal == "cat":
+            if (user_input_animal == "dog" or user_input_animal == "cat"
+                    or user_input_animal == "hamster" or user_input_animal == "guinea"):
                 try:
                     user_input_name_animal = input()
                     user_input_age_animal = input()
@@ -24,14 +40,14 @@ class Exercise3:
                         print("Incorrect input. Age <= 0")
                         user_num -= 1
                     else:
-                        if user_input_name_animal == "dog":
+                        if user_input_animal == "dog":
                             animal = self.Dog(user_input_name_animal, user_input_age_animal)
-                        elif user_input_name_animal == "cat":
+                        elif user_input_animal == "cat":
                             animal = self.Cat(user_input_name_animal, user_input_age_animal)
-                        elif user_input_name_animal == "hamster":
-                            animal = self.
+                        elif user_input_animal == "hamster":
+                            animal = self.Hamster(user_input_name_animal, user_input_age_animal)
                         else:
-                            animal = self
+                            animal = self.GuineaPig(user_input_name_animal, user_input_age_animal)
                         items_animals.append(animal)
                 except ValueError:
                     print("Could not parse a number. Please, try again")
@@ -41,52 +57,40 @@ class Exercise3:
 
         return items_animals
 
-    def print_pets(self, args):
-        for animal in args:
-            print(animal)
-
-    class harbinger(ABC):
-        @abstractmethod
-        def chill(self, message) -> str:
-            return message
-
-    class omnivore(ABC):
-        @abstractmethod
-        def hunt(self, message) -> str:
-            return message
-
 
     class AnimalABC(ABC):
         def __init__(self, name, age):
             self.name = name
             self.age = age
-
-        def get_name(self):
-            return self.name
-
-        def get_age(self):
-            return self.age
+        def get_name(self): return self.name
+        def get_age(self): return self.age
 
 
-    class Dog(AnimalABC, omnivore, ABC):
+    class Dog(AnimalABC, Omnivore):
         def __init__(self, name, age):
             super().__init__(name, age)
+        def __str__(self): return f"Dog name = {self.get_name()}, age = {self.get_age()}. {self.hunt()}"
+        def hunt(self) -> str: return "I can hunt for robbers"
 
-        def __str__(self):
-            return f"Dog name = {self.get_name()}, age = {self.get_age()}"
-
-        def hunt(self, message) -> str:
-            return "I can hunt for robbers"
-
-    class Cat(AnimalABC):
+    class Cat(AnimalABC, Omnivore):
         def __init__(self, name, age):
             super().__init__(name, age)
+        def __str__(self): return f"Cat name = {self.get_name()}, age = {self.get_age()}. {self.hunt()}"
+        def hunt(self) -> str: return "I can hunt for mice"
 
+    class Hamster(AnimalABC, Harbinger):
+        def __init__(self, name, age):
+            super().__init__(name, age)
+        def __str__(self): return f"Hamster name = {self.get_name()}, age = {self.get_age()}. {self.chill()}"
+        def chill(self) -> str: return "I can chill for 8 hours"
 
-        def __str__(self):
-            return f"Cat name = {self.get_name()}, age = {self.get_age()}"
+    class GuineaPig(AnimalABC, Harbinger):
+        def __init__(self, name, age):
+            super().__init__(name, age)
+        def chill(self) -> str: return "I can chill for 12 hours"
+        def __str__(self): return f"GuineaPig name = {self.get_name()}, age = {self.get_age()}. {self.chill()}"
 
 
 obj3 = Exercise3()
 items = obj3.input_user()
-obj3.print_pets(items)
+print_pets(items)
